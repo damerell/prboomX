sultim-t/prboom-plus-rt

commit 6811dbe6d3848b358eb8c0a2b50fcfe71e84eecc

A new version of the ammo patch linked above that tries to address fabian/myk's preferences by adding a 3-way option, "BACKPACK CHANGES THRESHOLDS" (and no, I could not think of a better name that fits on the screen) which is either "no", "full ammo only", or "yes". "no" is what fabian describes. "full ammo only" means that a backpack doesn't change red/green/yellow thresholds, but blue means full ammo. The default is "yes" because that is closest to the behaviour of prboom-plus pre-patch. It also now displays blue on the traditional HUD as appropriate (it doesn't display brown for no ammo, for reasons discussed beforehand.)
By David Damerell

http://www.doomworld.com/vb/post/1245168

prboom2/src/hu_stuff.c
@@ -1244,22 +1244,26 @@ int HU_GetArmorColor(int armor, int def)
   return result;
 }
 
-int HU_GetAmmoColor(int ammo, int fullammo, int def,int tofire)
+int HU_GetAmmoColor(int ammo, int fullammo, int def, int tofire, dboolean backpack)
 {
-  int result;
-
-  int ammopct = (100 * ammo) / fullammo;
+  int result, ammopct;
 
   if (ammo < tofire)
     result = CR_BROWN;
-  else if (ammo==fullammo)
+  else if ((ammo==fullammo) || 
+    (ammo_colour_behaviour == ammo_colour_behaviour_no && backpack && ammo*2 >= fullammo))
     result=def;
-  else if (ammopct < ammo_red)
-    result = CR_RED;
-  else if (ammopct < ammo_yellow)
-    result = CR_GOLD;
-  else
-    result = CR_GREEN;
+  else {
+    ammopct = (100 * ammo) / fullammo;
+    if (backpack && ammo_colour_behaviour != ammo_colour_behaviour_yes)
+      ammopct *= 2;
+    if (ammopct < ammo_red)
+      result = CR_RED;
+    else if (ammopct < ammo_yellow)
+      result = CR_GOLD;
+    else
+      result = CR_GREEN;
+  }
 
   return result;
 }
@@ -1315,8 +1319,8 @@ void HU_widget_build_ammo(void)
     strcat(hud_ammostr,ammostr);
 
     // set the display color from the percentage of total ammo held
-    w_ammo.cm = HU_GetAmmoColor(ammo, fullammo,CR_BLUE,
-				ammopershot[plr->readyweapon]);
+    w_ammo.cm = HU_GetAmmoColor(ammo, fullammo, CR_BLUE,
+      ammopershot[plr->readyweapon], plr->backpack);
   }
   // transfer the init string to the widget
   s = hud_ammostr;
@@ -1616,22 +1620,29 @@ void HU_widget_build_weapon(void)
     if (!plr->weaponowned[w])
       continue;
 
-    ammopct = fullammo? (100*ammo)/fullammo : 100;
-
     // display each weapon number in a color related to the ammo for it
     hud_weapstr[i++] = '\x1b'; //jff 3/26/98 use ESC not '\' for paths
     if (weaponinfo[w].ammo==am_noammo) //jff 3/14/98 show berserk on HUD
       hud_weapstr[i++] = plr->powers[pw_strength]? '0'+CR_GREEN : '0'+CR_GRAY;
-    else if (fullammo && ammo==fullammo)
-      hud_weapstr[i++] = '0'+CR_BLUE;
     else if (ammo<ammopershot[w])
       hud_weapstr[i++] = '0'+CR_BROWN;
-    else if (ammopct<ammo_red)
-      hud_weapstr[i++] = '0'+CR_RED;
-    else if (ammopct<ammo_yellow)
-      hud_weapstr[i++] = '0'+CR_GOLD;
+    else if (fullammo && ((ammo==fullammo) ||
+      (ammo_colour_behaviour == ammo_colour_behaviour_no &&
+      plr->backpack && ammo*2 >= fullammo)))
+      hud_weapstr[i++] = '0'+CR_BLUE;
     else
-      hud_weapstr[i++] = '0'+CR_GREEN;
+    {
+      ammopct = fullammo ? (100*ammo)/fullammo : 100;
+      if (plr->backpack && fullammo &&
+        ammo_colour_behaviour != ammo_colour_behaviour_yes)
+        ammopct *= 2;
+      if (ammopct<ammo_red)
+        hud_weapstr[i++] = '0'+CR_RED;
+      else if (ammopct<ammo_yellow)
+        hud_weapstr[i++] = '0'+CR_GOLD;
+      else
+        hud_weapstr[i++] = '0'+CR_GREEN;
+    }
     hud_weapstr[i++] = '0'+w+1;
     hud_weapstr[i++] = ' ';
     hud_weapstr[i] = '\0';
@@ -2062,8 +2073,8 @@ void HU_widget_build_ammo_big(void)
 
     // set the display color from the percentage of total ammo held
     if (!sts_always_red)
-      w_ammo_big.cm = HU_GetAmmoColor(ammo, fullammo,CR_BLUE2,
-				      ammopershot[plr->readyweapon]);
+      w_ammo_big.cm = HU_GetAmmoColor(ammo, fullammo, CR_BLUE2,
+        ammopershot[plr->readyweapon], plr->backpack);
 
     // transfer the init string to the widget
     s = ammostr;

prboom2/src/st_stuff.c
@@ -335,6 +335,13 @@ int armor_red;     // armor amount less than which status is red
 int armor_yellow;  // armor amount less than which status is yellow
 int armor_green;   // armor amount above is blue, below is green
 
+ammo_colour_behaviour_t ammo_colour_behaviour;
+const char *ammo_colour_behaviour_list[ammo_colour_behaviour_max] = {
+  "no",
+  "full ammo only",
+  "yes"
+};
+
  // in deathmatch only, summary of frags stats
 static st_number_t w_frags;
 
@@ -800,6 +807,7 @@ void M_ChangeApplyPalette(void)
 static void ST_drawWidgets(dboolean refresh)
 {
   int i;
+  int ammopct = 0;
 
   // used by w_arms[] widgets
   st_armson = st_statusbaron && !deathmatch;
@@ -808,15 +816,23 @@ static void ST_drawWidgets(dboolean refresh)
   st_fragson = deathmatch && st_statusbaron;
 
   //jff 2/16/98 make color of ammo depend on amount
-  if (*w_ready.num*100 < ammo_red*plyr->maxammo[weaponinfo[w_ready.data].ammo])
-    STlib_updateNum(&w_ready, CR_RED, refresh);
-  else
-    if (*w_ready.num*100 <
-        ammo_yellow*plyr->maxammo[weaponinfo[w_ready.data].ammo])
-      STlib_updateNum(&w_ready, CR_GOLD, refresh);
+  if ((*w_ready.num == plyr->maxammo[weaponinfo[w_ready.data].ammo]) ||
+    (ammo_colour_behaviour == ammo_colour_behaviour_no && plyr->backpack &&
+    *w_ready.num*2 >= plyr->maxammo[weaponinfo[w_ready.data].ammo]))
+    STlib_updateNum(&w_ready, CR_BLUE2, refresh);
+  else {
+    if (plyr->maxammo[weaponinfo[w_ready.data].ammo])
+      ammopct = (*w_ready.num*100)/plyr->maxammo[weaponinfo[w_ready.data].ammo];
+    if (plyr->backpack && ammo_colour_behaviour != ammo_colour_behaviour_yes)
+      ammopct *= 2;
+    if (ammopct < ammo_red)
+      STlib_updateNum(&w_ready, CR_RED, refresh);
     else
-      STlib_updateNum(&w_ready, CR_GREEN, refresh);
-
+      if (ammopct < ammo_yellow)
+        STlib_updateNum(&w_ready, CR_GOLD, refresh);
+      else
+        STlib_updateNum(&w_ready, CR_GREEN, refresh);
+  }
   for (i=0;i<4;i++)
     {
       STlib_updateNum(&w_ammo[i], CR_DEFAULT, refresh);   //jff 2/16/98 no xlation

prboom2/src/m_menu.c
@@ -50,6 +50,7 @@
 #include "w_wad.h"
 #include "r_main.h"
 #include "hu_stuff.h"
+#include "st_stuff.h"
 #include "g_game.h"
 #include "s_sound.h"
 #include "sounds.h"
@@ -2591,8 +2592,8 @@ void M_DrawWeapons(void)
 //
 // The Status Bar / HUD tables.
 
-#define ST_X 203
-#define ST_Y  31
+#define SB_X 203
+#define SB_Y  31
 
 // Screen table definitions
 
@@ -2610,28 +2611,30 @@ setup_menu_t* stat_settings[] =
 
 setup_menu_t stat_settings1[] =  // Status Bar and HUD Settings screen
 {
-  {"STATUS BAR"        ,S_SKIP|S_TITLE,m_null,ST_X,ST_Y+ 1*8 },
+  {"STATUS BAR"        ,S_SKIP|S_TITLE,m_null,SB_X,SB_Y+ 1*8 },
 
-  {"USE RED NUMBERS"   ,S_YESNO, m_null,ST_X,ST_Y+ 2*8, {"sts_always_red"}},
-  {"GRAY %"            ,S_YESNO, m_null,ST_X,ST_Y+ 3*8, {"sts_pct_always_gray"}},
-  {"SINGLE KEY DISPLAY",S_YESNO, m_null,ST_X,ST_Y+ 4*8, {"sts_traditional_keys"}},
+  {"USE RED NUMBERS"   ,S_YESNO, m_null,SB_X,SB_Y+ 2*8, {"sts_always_red"}},
+  {"GRAY %"            ,S_YESNO, m_null,SB_X,SB_Y+ 3*8, {"sts_pct_always_gray"}},
+  {"SINGLE KEY DISPLAY",S_YESNO, m_null,SB_X,SB_Y+ 4*8, {"sts_traditional_keys"}},
 
-  {"HEADS-UP DISPLAY"  ,S_SKIP|S_TITLE,m_null,ST_X,ST_Y+ 6*8},
+  {"HEADS-UP DISPLAY"  ,S_SKIP|S_TITLE,m_null,SB_X,SB_Y+ 6*8},
 
-  {"HEALTH LOW/OK"     ,S_NUM       ,m_null,ST_X,ST_Y+ 7*8, {"health_red"}},
-  {"HEALTH OK/GOOD"    ,S_NUM       ,m_null,ST_X,ST_Y+ 8*8, {"health_yellow"}},
-  {"HEALTH GOOD/EXTRA" ,S_NUM       ,m_null,ST_X,ST_Y+ 9*8, {"health_green"}},
-  {"ARMOR LOW/OK"      ,S_NUM       ,m_null,ST_X,ST_Y+10*8, {"armor_red"}},
-  {"ARMOR OK/GOOD"     ,S_NUM       ,m_null,ST_X,ST_Y+11*8, {"armor_yellow"}},
-  {"ARMOR GOOD/EXTRA"  ,S_NUM       ,m_null,ST_X,ST_Y+12*8, {"armor_green"}},
-  {"AMMO LOW/OK"       ,S_NUM       ,m_null,ST_X,ST_Y+13*8, {"ammo_red"}},
-  {"AMMO OK/GOOD"      ,S_NUM       ,m_null,ST_X,ST_Y+14*8, {"ammo_yellow"}},
+  {"HEALTH LOW/OK"     ,S_NUM       ,m_null,SB_X,SB_Y+ 7*8, {"health_red"}},
+  {"HEALTH OK/GOOD"    ,S_NUM       ,m_null,SB_X,SB_Y+ 8*8, {"health_yellow"}},
+  {"HEALTH GOOD/EXTRA" ,S_NUM       ,m_null,SB_X,SB_Y+ 9*8, {"health_green"}},
+  {"ARMOR LOW/OK"      ,S_NUM       ,m_null,SB_X,SB_Y+10*8, {"armor_red"}},
+  {"ARMOR OK/GOOD"     ,S_NUM       ,m_null,SB_X,SB_Y+11*8, {"armor_yellow"}},
+  {"ARMOR GOOD/EXTRA"  ,S_NUM       ,m_null,SB_X,SB_Y+12*8, {"armor_green"}},
+  {"AMMO LOW/OK"       ,S_NUM       ,m_null,SB_X,SB_Y+13*8, {"ammo_red"}},
+  {"AMMO OK/GOOD"      ,S_NUM       ,m_null,SB_X,SB_Y+14*8, {"ammo_yellow"}},
+  {"BACKPACK CHANGES THRESHOLDS",S_CHOICE,m_null,SB_X,SB_Y+15*8, 
+   {"ammo_colour_behaviour"},0,0,NULL,ammo_colour_behaviour_list},
 
   // Button for resetting to defaults
   {0,S_RESET,m_null,X_BUTTON,Y_BUTTON},
 
   //e6y
-  {"NEXT ->",S_SKIP|S_NEXT,m_null,KB_NEXT,ST_Y+20*8, {stat_settings2}},
+  {"NEXT ->",S_SKIP|S_NEXT,m_null,KB_NEXT,SB_Y+20*8, {stat_settings2}},
   // Final entry
   {0,S_SKIP|S_END,m_null}
 };
@@ -2640,25 +2643,25 @@ setup_menu_t stat_settings1[] =  // Status Bar and HUD Settings screen
 #define ADVHUD_X 284
 setup_menu_t stat_settings2[] =
 {
-  {"ADVANCED HUD SETTINGS"       ,S_SKIP|S_TITLE,m_null,ADVHUD_X,ST_Y+1*8},
-  {"SECRET AREAS"                ,S_YESNO     ,m_null,ADVHUD_X,ST_Y+ 2*8, {"hudadd_secretarea"}},
-  {"SMART TOTALS"                ,S_YESNO     ,m_null,ADVHUD_X,ST_Y+ 3*8, {"hudadd_smarttotals"}},
-  {"SHOW GAMESPEED"              ,S_YESNO     ,m_null,ADVHUD_X,ST_Y+ 4*8, {"hudadd_gamespeed"}},
-  {"SHOW LEVELTIME"              ,S_YESNO     ,m_null,ADVHUD_X,ST_Y+ 5*8, {"hudadd_leveltime"}},
-  {"SHOW DEMOTIME"               ,S_YESNO     ,m_null,ADVHUD_X,ST_Y+ 6*8, {"hudadd_demotime"}},
-  {"SHOW PROGRESS BAR DURING DEMO PLAYBACK" ,S_YESNO     ,m_null,ADVHUD_X,ST_Y+ 7*8, {"hudadd_demoprogressbar"}},
+  {"ADVANCED HUD SETTINGS"       ,S_SKIP|S_TITLE,m_null,ADVHUD_X,SB_Y+1*8},
+  {"SECRET AREAS"                ,S_YESNO     ,m_null,ADVHUD_X,SB_Y+ 2*8, {"hudadd_secretarea"}},
+  {"SMART TOTALS"                ,S_YESNO     ,m_null,ADVHUD_X,SB_Y+ 3*8, {"hudadd_smarttotals"}},
+  {"SHOW GAMESPEED"              ,S_YESNO     ,m_null,ADVHUD_X,SB_Y+ 4*8, {"hudadd_gamespeed"}},
+  {"SHOW LEVELTIME"              ,S_YESNO     ,m_null,ADVHUD_X,SB_Y+ 5*8, {"hudadd_leveltime"}},
+  {"SHOW DEMOTIME"               ,S_YESNO     ,m_null,ADVHUD_X,SB_Y+ 6*8, {"hudadd_demotime"}},
+  {"SHOW PROGRESS BAR DURING DEMO PLAYBACK" ,S_YESNO     ,m_null,ADVHUD_X,SB_Y+ 7*8, {"hudadd_demoprogressbar"}},
 
-  {"CROSSHAIR SETTINGS"            ,S_SKIP|S_TITLE,m_null,ADVHUD_X,ST_Y+9*8},
-  {"ENABLE CROSSHAIR"              ,S_CHOICE   ,m_null,ADVHUD_X,ST_Y+10*8, {"hudadd_crosshair"}, 0, 0, 0, crosshair_str},
-  {"SCALE CROSSHAIR"               ,S_YESNO    ,m_null,ADVHUD_X,ST_Y+11*8, {"hudadd_crosshair_scale"}},
-  {"CHANGE CROSSHAIR COLOR BY PLAYER HEALTH" ,S_YESNO    ,m_null,ADVHUD_X,ST_Y+12*8, {"hudadd_crosshair_health"}},
-  {"CHANGE CROSSHAIR COLOR ON TARGET"        ,S_YESNO    ,m_null,ADVHUD_X,ST_Y+13*8, {"hudadd_crosshair_target"}},
-  {"LOCK CROSSHAIR ON TARGET"                ,S_YESNO    ,m_null,ADVHUD_X,ST_Y+14*8, {"hudadd_crosshair_lock_target"}},
-  {"DEFAULT CROSSHAIR COLOR"                 ,S_CRITEM   ,m_null,ADVHUD_X,ST_Y+15*8, {"hudadd_crosshair_color"}},
-  {"TARGET CROSSHAIR COLOR"                  ,S_CRITEM   ,m_null,ADVHUD_X,ST_Y+16*8, {"hudadd_crosshair_target_color"}},
+  {"CROSSHAIR SETTINGS"            ,S_SKIP|S_TITLE,m_null,ADVHUD_X,SB_Y+9*8},
+  {"ENABLE CROSSHAIR"              ,S_CHOICE   ,m_null,ADVHUD_X,SB_Y+10*8, {"hudadd_crosshair"}, 0, 0, 0, crosshair_str},
+  {"SCALE CROSSHAIR"               ,S_YESNO    ,m_null,ADVHUD_X,SB_Y+11*8, {"hudadd_crosshair_scale"}},
+  {"CHANGE CROSSHAIR COLOR BY PLAYER HEALTH" ,S_YESNO    ,m_null,ADVHUD_X,SB_Y+12*8, {"hudadd_crosshair_health"}},
+  {"CHANGE CROSSHAIR COLOR ON TARGET"        ,S_YESNO    ,m_null,ADVHUD_X,SB_Y+13*8, {"hudadd_crosshair_target"}},
+  {"LOCK CROSSHAIR ON TARGET"                ,S_YESNO    ,m_null,ADVHUD_X,SB_Y+14*8, {"hudadd_crosshair_lock_target"}},
+  {"DEFAULT CROSSHAIR COLOR"                 ,S_CRITEM   ,m_null,ADVHUD_X,SB_Y+15*8, {"hudadd_crosshair_color"}},
+  {"TARGET CROSSHAIR COLOR"                  ,S_CRITEM   ,m_null,ADVHUD_X,SB_Y+16*8, {"hudadd_crosshair_target_color"}},
 
   {0,S_RESET,m_null,X_BUTTON,Y_BUTTON},
-  {"<- PREV",S_SKIP|S_PREV,m_null,KB_PREV,ST_Y+20*8, {stat_settings1}},
+  {"<- PREV",S_SKIP|S_PREV,m_null,KB_PREV,SB_Y+20*8, {stat_settings1}},
   {0,S_SKIP|S_END,m_null}
 };
 
@@ -3235,7 +3238,7 @@ setup_menu_t gen_settings6[] =
   {"WARN ON MISSEDBACKSIDE OVERFLOW"   ,S_YESNO     ,m_null,G_X2,G_Y+12*8, {"overrun_missedbackside_warn"}},
   {"TRY TO EMULATE IT"                 ,S_YESNO     ,m_null,G_X2,G_Y+13*8, {"overrun_missedbackside_emulate"}},
 
-  {"<- PREV",S_SKIP|S_PREV,m_null,KB_PREV,ST_Y+20*8, {gen_settings5}},
+  {"<- PREV",S_SKIP|S_PREV,m_null,KB_PREV,KB_Y+20*8, {gen_settings5}},
   {"NEXT ->",S_SKIP|S_NEXT,m_null,KB_NEXT,KB_Y+20*8, {gen_settings7}},
   {0,S_SKIP|S_END,m_null}
 };
@@ -3249,7 +3252,7 @@ setup_menu_t gen_settings7[] =
   {"FIX CLIPPING PROBLEMS IN LARGE LEVELS" ,S_YESNO ,m_null,G_X2,G_Y+5*8, {"comperr_blockmap"}},
   {"ALLOW JUMP"                        ,S_YESNO     ,m_null,G_X2,G_Y+6*8, {"comperr_allowjump"}},
 
-  {"<- PREV",S_SKIP|S_PREV,m_null,KB_PREV,ST_Y+20*8, {gen_settings6}},
+  {"<- PREV",S_SKIP|S_PREV,m_null,KB_PREV,KB_Y+20*8, {gen_settings6}},
 #ifdef GL_DOOM
   {"NEXT ->",S_SKIP|S_NEXT,m_null,KB_NEXT,KB_Y+20*8, {gen_settings8}},
 #endif

prboom2/src/m_misc.c
@@ -868,6 +868,9 @@ default_t defaults[] =
    def_int,ss_stat}, // percent of ammo for red to yellow transition
   {"ammo_yellow",   {&ammo_yellow}  , {50},0,100, // below 50% is yellow, above green
    def_int,ss_stat}, // percent of ammo for yellow to green transition
+  {"ammo_colour_behaviour",{(int*)&ammo_colour_behaviour},
+   {ammo_colour_behaviour_max-1}, // whether backpack changes thresholds above
+   0,ammo_colour_behaviour_max-1,def_int,ss_stat},
 
   //jff 2/16/98 HUD and status feature controls
   {"hud_num",    {&hud_num}, {6},0,100,

prboom2/src/st_stuff.h
@@ -106,6 +106,15 @@ extern int sts_traditional_keys;  // display keys the traditional way
 
 extern int st_palette;    // cph 2006/04/06 - make palette visible
 
+typedef enum {
+  ammo_colour_behaviour_no,
+  ammo_colour_behaviour_full_only,
+  ammo_colour_behaviour_yes,
+  ammo_colour_behaviour_max
+} ammo_colour_behaviour_t;
+extern ammo_colour_behaviour_t ammo_colour_behaviour;
+extern const char *ammo_colour_behaviour_list[];
+
 // e6y: makes sense for wide resolutions
 extern patchnum_t grnrock;
 extern patchnum_t brdr_t, brdr_b, brdr_l, brdr_r;

damerell/prboomX

commit a138e4b46e46a9459f4275ef8c37fe7043f99b49

Fix rounding error in ammo colours
commit 6811dbe6d3848b358eb8c0a2b50fcfe71e84eecc (written by me, oops)
introduced a rounding error most easily seen by setting option
ammo_color_behavior to "no" and ammo_red or ammo_yellow to an odd
number and firing off chaingun ammo; the colour displayed on the HUD /
status bar will change too early. This is because integer division rounds
down before the "ammopct *= 2".

This commit fixes that bug. Sorry I spotted it in, er, 2016 and then
forgot about it for nine years.

prboom2/src/hu_stuff.c
@@ -1399,9 +1399,9 @@ int HU_GetAmmoColor(int ammo, int fullammo, int def, int tofire, dboolean backpa
     (ammo_colour_behaviour == ammo_colour_behaviour_no && backpack && ammo*2 >= fullammo))
     result=def;
   else {
-    ammopct = (100 * ammo) / fullammo;
-    if (backpack && ammo_colour_behaviour != ammo_colour_behaviour_yes)
-      ammopct *= 2;
+      ammopct = (((backpack &&
+		   (ammo_colour_behaviour != ammo_colour_behaviour_yes))
+		  ? 200 : 100) * ammo) / fullammo;
     if (ammopct < ammo_red)
       result = CR_RED;
     else if (ammopct < ammo_yellow)
@@ -1775,12 +1775,17 @@ void HU_widget_build_weapon(void)
       (ammo_colour_behaviour == ammo_colour_behaviour_no &&
       plr->backpack && ammo*2 >= fullammo)))
       hud_weapstr[i++] = '0'+CR_BLUE;
-    else
-    {
-      ammopct = fullammo ? (100*ammo)/fullammo : 100;
-      if (plr->backpack && fullammo &&
-        ammo_colour_behaviour != ammo_colour_behaviour_yes)
-        ammopct *= 2;
+    else {
+      if (fullammo) {
+	ammopct = (plr->backpack &&
+		   ammo_colour_behaviour != ammo_colour_behaviour_yes) ?
+	200 : 100;
+	ammopct *=ammo; ammopct /= fullammo;
+      } else {
+// I'm not sure we can get here - are there weapons where fullammo = 0 but
+// not weaponinfo[w].ammo==am_noammo? But it's been this way since 2010...
+	ammopct = 100; 
+      }
       if (ammopct<ammo_red)
         hud_weapstr[i++] = '0'+CR_RED;
       else if (ammopct<ammo_yellow)

prboom2/src/st_stuff.c
@@ -858,9 +858,10 @@ static void ST_drawWidgets(dboolean refresh)
     STlib_updateNum(&w_ready, CR_BLUE2, refresh);
   else {
     if (plyr->maxammo[weaponinfo[w_ready.data].ammo])
-      ammopct = (*w_ready.num*100)/plyr->maxammo[weaponinfo[w_ready.data].ammo];
-    if (plyr->backpack && ammo_colour_behaviour != ammo_colour_behaviour_yes)
-      ammopct *= 2;
+	ammopct = (((plyr->backpack &&
+		     (ammo_colour_behaviour != ammo_colour_behaviour_yes))
+		    ? 200: 100) * *w_ready.num /
+		   plyr->maxammo[weaponinfo[w_ready.data].ammo]);
     if (ammopct < ammo_red)
       STlib_updateNum(&w_ready, CR_RED, refresh);
     else